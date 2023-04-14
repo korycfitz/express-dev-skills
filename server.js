552e98d7d807d34bebecf4520d355d67d7177aae
korycfitz <korycfitz@gmail.com>
2023-04-13 23:42:41 -0400

    create env file, link to database, import dotenv in server.js and install corresponding npm packages to import dotenv and mongoose npm packages

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import createError from 'http-errors'
 import logger from 'morgan'
-
+import "dotenv/config.js"
 // import routers
 import { router as indexRouter } from './routes/index.js'
 import { router as notesRouter } from './routes/notes.js'
